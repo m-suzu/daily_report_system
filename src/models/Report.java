30973694aebf1044e35cd1b7abc213ebc835f441
m.suzuki <0330@gmail.com>
2021-05-31 17:32:45 +0900

    Add Report create

--- src/models/Report.java
+++ src/models/Report.java
@@ -52,7 +52,7 @@ public class Report {
     private Timestamp created_at;
 
     @Column(name = "updated_at", nullable = false)
-    private Timestamp update_at;
+    private Timestamp updated_at;
 
     public Integer getId() {
         return id;
@@ -102,12 +102,12 @@ public class Report {
         this.created_at = created_at;
     }
 
-    public Timestamp getUpdate_at() {
-        return update_at;
+    public Timestamp getUpdated_at() {
+        return updated_at;
     }
 
-    public void setUpdate_at(Timestamp update_at) {
-        this.update_at = update_at;
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
 
 }
